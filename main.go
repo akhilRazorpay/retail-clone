b4b35ab05b0b9f2391b8f5b028c61b7de7ab0d27
akhil <gandikota.akhil@razorpay.com>
2022-08-02 19:08:40 +0530

    added configurations

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"retail-clone/controllers"
+	"retail-clone/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	A := gin.Default()
 
-	fmt.Println("hello world")
+	// Connect to database
+	models.ConnectDatabase()
+
+	// Routes
+	A.GET("/products", controllers.Findproducts)
+	A.GET("/products/:id", controllers.Findproduct)
+	A.POST("/products", controllers.Createproduct)
+	A.PATCH("/products/:id", controllers.Updateproduct)
+	A.DELETE("/products/:id", controllers.Deleteproduct)
+
+	// Run the server
+	A.Run()
 }
